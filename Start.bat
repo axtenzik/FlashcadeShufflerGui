53d79b858c6e75fa199428365185e8bd6ac7fe9e
Brandon Verducci <brverducci@live.co.uk>
2022-01-06 15:18:48 +0000

    some changes

--- Start.bat
+++ Start.bat
@@ -1,2 +1,5 @@
-ECHO Hello World
-Set /p INPUT=
+SET HOST=127.0.0.1
+SET PORT=7070
+SET BIZHAWK_PATH=.\
+
+start /B %BIZHAWK_PATH%\\EmuHawk.exe --socket_ip=%HOST% --socket_port=%PORT% --lua=%~dp0FlashcadeShuffler.lua
